83631f9a61bd404caf5a5a87b2e3538390124946
Thiago Kaique <66787043+Thiago099@users.noreply.github.com>
2022-04-01 08:26:26 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
 const mysql = require('mysql');
 
+const reset = "\x1b[0m"
+const bright = "\x1b[1m"
+const dim = "\x1b[2m"
+const underscore = "\x1b[4m"
+const blink = "\x1b[5m"
+const reverse = "\x1b[7m"
+const hidden = "\x1b[8m"
+
+const black = "\x1b[30m"
+const red = "\x1b[31m"
+const green = "\x1b[32m"
+const yellow = "\x1b[33m"
+const blue = "\x1b[34m"
+const magenta = "\x1b[35m"
+const cyan = "\x1b[36m"
+const white = "\x1b[37m"
+
 async function rodaQueries()
 {
   const BANCOS = process.argv.splice(3);
@@ -16,13 +33,13 @@ async function rodaQueries()
           // use database
               CONEXAO.query(process.argv[2], (erro, resultado) => {
                 if(erro){
-                  console.log(banco + ': '+erro);
+                  console.log( `${cyan}${BANCOS[banco]}${reset}: ${red}${erro}${reset}`);
                 }
                 else{
                   // check if it is a query
                   if(resultado.constructor.name == 'OkPacket')
                     {
-                      console.log(banco + ': sucesso, '+resultado.affectedRows + ' linhas afetadas');
+                      console.log(`${cyan}${BANCOS[banco]}${reset}: ${green}sucesso, ${resultado.affectedRows} linhas afetadas${reset}`);
                     }
                     else
                     {
